87a84d15ea2a22e602619a5e6babf9debfab77fa
jesus solano <b425f92jv7@privaterelay.appleid.com>
2025-11-30 22:00:24 -0500

    Add files via upload

---
+++ km-mi.py
@@ -0,0 +1,23 @@
+#*****km a millas*****
+#*****zona funcional*****
+
+def capturar_km():
+    km = int(input("digite el valor de los kilometros que desea convertir: ")) 
+    return km
+
+def hacer_convercion(km):
+    conversion= (km*0.621)
+    return conversion
+
+def hacer_mesaje(km, conversion ):
+    mensaje = str(km) + " km convertido a millas es: " + str(conversion)
+    return mensaje
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)
+
+#****zona codigo principal*****
+km = capturar_km()
+conversion = hacer_convercion(km)
+mensaje = hacer_mesaje(km, conversion)
+imprimir = imprimir_mensaje(mensaje)
