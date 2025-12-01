2c9bc8de3ecc31abd61af25adca5b681d97742d3
jesus solano <b425f92jv7@privaterelay.appleid.com>
2025-11-30 21:58:44 -0500

    Add files via upload

---
+++ area de paralelogramo.py
@@ -0,0 +1,29 @@
+#******area de un paralelogramo ******
+#*******zona de funciones ********
+def capturar_base ():
+    base = int(input("digite el valor de la base: "))
+    return base
+
+def capturar_altura ():
+    altura = int(input("digite el valor de la altura: "))
+    return altura
+
+def hacer_area (base, altura):
+    area = (base * altura )
+    return area
+
+def hacer_mensaje(area):
+    mensaje = "la area del paralelogramo es: " + str(area)
+    return mensaje 
+
+def imprimir_mensaje(mensaje):
+    print (mensaje)
+    
+    
+#***** zona codigo principal*******
+
+base = capturar_base()
+altura = capturar_altura()
+area = hacer_area(base, altura)
+mensaje = hacer_mensaje(area)
+imprimir = imprimir_mensaje(mensaje)
