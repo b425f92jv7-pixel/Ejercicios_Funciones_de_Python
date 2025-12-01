413c033fb44d7fb02da5444236c2c1bf1d5a4228
jesus solano <b425f92jv7@privaterelay.appleid.com>
2025-11-30 22:00:42 -0500

    Add files via upload

---
+++ libras a kilos.py
@@ -0,0 +1,23 @@
+#*****libras a kilos*****
+#*****zona funcional*****
+
+def capturar_libras():
+    libras = int(input("digite el valor de las libras que desea convertir: ")) 
+    return libras
+
+def hacer_convercion(libras):
+    conversion= (libras * 0.454)
+    return conversion
+
+def hacer_mesaje(libras, conversion ):
+    mensaje = str(libras) + " km convertido a millas es: " + str(conversion)
+    return mensaje
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)
+
+#****zona codigo principal*****
+libras = capturar_libras()
+conversion = hacer_convercion(libras)
+mensaje = hacer_mesaje(libras, conversion)
+imprimir = imprimir_mensaje(mensaje)
