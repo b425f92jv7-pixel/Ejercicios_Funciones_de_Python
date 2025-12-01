fb19f81baaa834458a9c385bf60c198be6eb9cd9
jesus solano <b425f92jv7@privaterelay.appleid.com>
2025-11-30 21:58:25 -0500

    Add files via upload

---
+++ area cuadrado.py
@@ -0,0 +1,22 @@
+#******area cuadrado ******
+#*******zona de funciones ********
+def capturar_lado ():
+    lado = int(input("digite el valor del lado: "))
+    return lado
+
+def hacer_area(lado):
+    area= (lado * lado)
+    return area
+
+def hacer_mesaje(area ):
+    mensaje = "la area del cuadrado es: " + str(area)
+    return mensaje
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)
+    
+#****zona codigo principal*****
+lado = capturar_lado()
+area = hacer_area(lado)
+mensaje = hacer_mesaje(area)
+imprimir = imprimir_mensaje(mensaje)
