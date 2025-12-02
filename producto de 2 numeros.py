b9510774170f002cf87f1fb8ad4c53b9ab27eac1
jesus solano <b425f92jv7@privaterelay.appleid.com>
2025-12-02 09:07:47 -0500

    Add files via upload

---
+++ producto de 2 numeros.py
@@ -0,0 +1,29 @@
+#*****solicite 2 y de su producto****
+#****zona funcional*****
+
+def capturar_n1():
+    numero1= int(input("ingrese el primer numero: "))
+    return numero1
+
+def capturar_n2():
+    numero2= int(input("ingrese el segundo numero: "))
+    return numero2
+
+def sumar_ns (numero1, numero2):
+    mult = (numero1 * numero2)
+    return mult
+
+def hacer_mensaje(mult):
+    mensaje = "el producto de los numeros es: " + str(mult)
+    return mensaje
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)  
+    
+  
+#****codigo principal*****    
+n1 = capturar_n1()
+n2 = capturar_n2()
+mult = sumar_ns(n1, n2)
+mensaje = hacer_mensaje(mult)
+imprimir = imprimir_mensaje(mensaje)
