3515f0786727ea9346b288846c0fee3f799692c1
jesus solano <b425f92jv7@privaterelay.appleid.com>
2025-12-05 06:34:14 -0500

    Add files via upload

---
+++ cuadrado de un numero.py
@@ -0,0 +1,25 @@
+#******cuadrado de un numero******
+
+def capturar_n():
+    numero= int(input("ingrese el numero: "))
+    return numero
+
+
+def sumar_ns (numero):
+    mult = (numero * numero)
+    return mult
+
+def hacer_mensaje(mult):
+    mensaje = "el cuadrado del numero es: " + str(mult)
+    return mensaje
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)  
+    
+  
+#****codigo principal*****    
+n= capturar_n()
+
+mult = sumar_ns(n)
+mensaje = hacer_mensaje(mult)
+imprimir = imprimir_mensaje(mensaje)
